3be6b0a5c853d3b472c6a6a15380d5a6ed8a383c
Khushi <68914425+KhushiSri12@users.noreply.github.com>
2021-12-13 11:45:06 +0530

    Add files via upload

---
+++ get subsequence.java
@@ -0,0 +1,39 @@
+/*
+PEPCODING LEVEL-1 RECURSION WITH ARRAYLISTS 
+
+GET SUBSEQUENCE
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+           Scanner sc=new Scanner(System.in);
+           String str=sc.next();
+           ArrayList<String> res=gss(str);
+           System.out.println
+           (res);
+    }
+
+    public static ArrayList<String> gss(String str) {
+        if(str.length()==0){
+            ArrayList<String> bres=new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+        char ch=str.charAt(0);
+        String ros=str.substring(1);
+        ArrayList<String> rres=gss(ros);
+        ArrayList<String> ares=new ArrayList<>();
+        for(String rstr:rres){
+            ares.add(""+rstr);
+        }
+        for(String rstr:rres){
+            ares.add(ch+rstr);
+        }
+        return ares;
+    }
+
+}
